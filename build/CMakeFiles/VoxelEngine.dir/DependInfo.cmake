11263110506b5b0dfe683593b866e7bf966646f0
deanpr02 <deanprach@gmail.com>
2025-03-25 16:53:21 -0700

    misc. additions

---
+++ build/CMakeFiles/VoxelEngine.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/deanp/OneDrive/Documents/VoxelEngine/src/glad.c" "CMakeFiles/VoxelEngine.dir/src/glad.c.obj" "gcc" "CMakeFiles/VoxelEngine.dir/src/glad.c.obj.d"
+  "C:/Users/deanp/OneDrive/Documents/VoxelEngine/src/camera.cpp" "CMakeFiles/VoxelEngine.dir/src/camera.cpp.obj" "gcc" "CMakeFiles/VoxelEngine.dir/src/camera.cpp.obj.d"
+  "C:/Users/deanp/OneDrive/Documents/VoxelEngine/src/chunk_manager.cpp" "CMakeFiles/VoxelEngine.dir/src/chunk_manager.cpp.obj" "gcc" "CMakeFiles/VoxelEngine.dir/src/chunk_manager.cpp.obj.d"
+  "C:/Users/deanp/OneDrive/Documents/VoxelEngine/src/engine.cpp" "CMakeFiles/VoxelEngine.dir/src/engine.cpp.obj" "gcc" "CMakeFiles/VoxelEngine.dir/src/engine.cpp.obj.d"
+  "C:/Users/deanp/OneDrive/Documents/VoxelEngine/src/generator.cpp" "CMakeFiles/VoxelEngine.dir/src/generator.cpp.obj" "gcc" "CMakeFiles/VoxelEngine.dir/src/generator.cpp.obj.d"
+  "C:/Users/deanp/OneDrive/Documents/VoxelEngine/src/main.cpp" "CMakeFiles/VoxelEngine.dir/src/main.cpp.obj" "gcc" "CMakeFiles/VoxelEngine.dir/src/main.cpp.obj.d"
+  "C:/Users/deanp/OneDrive/Documents/VoxelEngine/src/physics.cpp" "CMakeFiles/VoxelEngine.dir/src/physics.cpp.obj" "gcc" "CMakeFiles/VoxelEngine.dir/src/physics.cpp.obj.d"
+  "C:/Users/deanp/OneDrive/Documents/VoxelEngine/src/player.cpp" "CMakeFiles/VoxelEngine.dir/src/player.cpp.obj" "gcc" "CMakeFiles/VoxelEngine.dir/src/player.cpp.obj.d"
+  "C:/Users/deanp/OneDrive/Documents/VoxelEngine/src/q_math.cpp" "CMakeFiles/VoxelEngine.dir/src/q_math.cpp.obj" "gcc" "CMakeFiles/VoxelEngine.dir/src/q_math.cpp.obj.d"
+  "C:/Users/deanp/OneDrive/Documents/VoxelEngine/src/renderer.cpp" "CMakeFiles/VoxelEngine.dir/src/renderer.cpp.obj" "gcc" "CMakeFiles/VoxelEngine.dir/src/renderer.cpp.obj.d"
+  "C:/Users/deanp/OneDrive/Documents/VoxelEngine/src/shader.cpp" "CMakeFiles/VoxelEngine.dir/src/shader.cpp.obj" "gcc" "CMakeFiles/VoxelEngine.dir/src/shader.cpp.obj.d"
+  "C:/Users/deanp/OneDrive/Documents/VoxelEngine/src/spells.cpp" "CMakeFiles/VoxelEngine.dir/src/spells.cpp.obj" "gcc" "CMakeFiles/VoxelEngine.dir/src/spells.cpp.obj.d"
+  "C:/Users/deanp/OneDrive/Documents/VoxelEngine/src/stb_image.cpp" "CMakeFiles/VoxelEngine.dir/src/stb_image.cpp.obj" "gcc" "CMakeFiles/VoxelEngine.dir/src/stb_image.cpp.obj.d"
+  "C:/Users/deanp/OneDrive/Documents/VoxelEngine/src/weapon.cpp" "CMakeFiles/VoxelEngine.dir/src/weapon.cpp.obj" "gcc" "CMakeFiles/VoxelEngine.dir/src/weapon.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
